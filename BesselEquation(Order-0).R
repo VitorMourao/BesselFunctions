fdda90907c3471329f0d50b0857764879b555d1a
VitorMourao <vitor_mourao@live.com>
2019-07-24 16:22:20 -0300

    Update with the comparison BesselJ
    In addition to adding the comparison, some explanatory comments were also inserted

--- BesselEquation(Order-0).R
+++ BesselEquation(Order-0).R
@@ -1,12 +1,16 @@
 library(deSolve) #import library deSolve
 
 #Initiate the initial values:
-st <- c(y1 = 1, dy1 = 0) #J(0) = 1 and J'(0) = 0 (Bessel order 0)
+st <- c(y1 = 1, dy1 = 0)
+#y1 = 0.005, dy1 = 1/2 for order 1.
 
-#Insert the Bessel function (order 0): (t^2)y'' + ty' + (t^2 - k^2)y = 0, k = 0
-BesselFunction <- function(t,y,p){ 
+#Order of the Bessel Equation
+k <- 0
+
+#Insert the Bessel function : (x^2)y'' + xy' + (x^2 - k^2)y = 0, k = 0
+BesselFunction <- function(x,y,p){ 
   dy1 <- y[2] #y' = y[2]
-  dy2 <- -((t^2)*y[1]+(t*y[2]))/(t^2) #Isolate y[2]'
+  dy2 <- -(((x^2)-k^2)*y[1]+(x*y[2]))/(x^2) #Isolate y[2]'
   list(c(dy1,dy2))
 }
 
@@ -16,13 +20,17 @@ times <- seq(from = 0.01, to = 50, by = 0.01)
 #Using the function ode from deSolve to solve the problem
 out <- ode(y = st, func = BesselFunction, times = times)
 
-#Plot the function.
-plot(times, out[,"y1"], type = "l", xlab = "Time", ylab = "Y", lwd = 2,
-     main = "Bessel Function of order 0")
-
 #To view the first 10 iterations:
 head(out, n = 10)
 #To view the last 10 iterations:
 tail(out, n = 10)
 #To view between 100 iterations and 120 iterations:
 out[100:120, c("time", "y1")]
+
+#Bessel function of the first kind in the library (Base) in R
+Jzero <- besselJ(times, k)
+
+#Plot the function.
+plot(times, out[,"y1"], type = "l", xlab = "x", ylab = "J(x)", lwd = 3,
+     main = "Bessel Function", col = "black")
+lines(times, Jzero, type = "l", col = "red", lwd = 1.5)
